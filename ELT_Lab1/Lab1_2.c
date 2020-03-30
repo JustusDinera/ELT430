198b396dae848d97ec1053d6bba7934ccaef38f0
Justus TOWER <justus@darmsign.al>
2020-03-30 16:45:45 +0200

    test ccs

--- ELT_Lab1/Lab1_2.c
+++ ELT_Lab1/Lab1_2.c
@@ -1,12 +1,14 @@
 /*
  * Lab1_1.c
  *
- *  Created on: 11.03.2020
- *      Author: dinera
+ *  Created on:     11.03.2020
+ *  Author:         Justus Weinhold
+ *  Description:    Wechselseitiges Blinken der 4 LEDs auf dem Zusatzboard im 0,5 Sekunden Takt
  */
 
 #include "DSP28x_Project.h"
 void main(void) {
+    unsigned long i;                        // Timer Zaehlvariable deklarieren
     EALLOW;
     GpioCtrlRegs.GPADIR.bit.GPIO17 = 1;     //Ausgang setzen
     GpioCtrlRegs.GPBDIR.all |= 0x8C0000;     // Ausgang setzen
@@ -16,7 +18,6 @@ void main(void) {
     GpioDataRegs.GPBCLEAR.bit.GPIO50 = 1;   // LED P2 aus
     GpioDataRegs.GPBSET.bit.GPIO51 = 1;     // LED P3 ein
     GpioDataRegs.GPBCLEAR.bit.GPIO55 = 1;   // LED P4 aus
-    unsigned long i;                        // Timer Zaehlvariable deklarieren
 
     while(1)
     {
